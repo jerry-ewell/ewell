6e26d334a9f87eb1e4fd86666fb1f571315e81f5
nova <nova@ewell.finance>
2024-01-15 14:24:51 +0800

    chore: mock data

--- src/pages/ProjectInfo/newComponents/InfoWrapper/index.tsx
+++ src/pages/ProjectInfo/newComponents/InfoWrapper/index.tsx
@@ -1,30 +1,20 @@
-import { useMemo } from 'react';
 import ProjectLogo from 'components/ProjectLogo';
 import CommonLink from 'components/CommonLink';
 import IconFont from 'components/IconFont';
 import ProjectTabs from '../ProjectTabs';
 import { InfoIcons } from 'constants/iconfont';
-import { useProjectInfo } from 'contexts/useProjectInfo';
 import { getHref } from 'utils';
 import './styles.less';
 
 export default function InfoWrapper() {
-  const [{ idoInfo }] = useProjectInfo();
-
-  const additionalInfo = useMemo(() => idoInfo?.additionalInfo, [idoInfo?.additionalInfo]);
-
   return (
     <div className="info-wrapper flex flex-1">
-      <ProjectLogo
-        key={additionalInfo?.logoUrl}
-        src={additionalInfo?.logoUrl}
-        alt={idoInfo?.crowdFundingIssueToken?.symbol}
-      />
+      <ProjectLogo src="" alt="logo" />
       <div className="info flex-1 flex-column">
         <div className="info-header flex-column">
-          <div className="project-name">{idoInfo?.crowdFundingIssueToken?.name ?? '--'}</div>
+          <div className="project-name">Citizen Conflict</div>
           <div className="media-wrapper flex-row-center flex-wrap">
-            {Object.entries(additionalInfo ?? {}).map(([key, value]) => {
+            {[].map(([key, value]) => {
               const type = InfoIcons[key as keyof typeof InfoIcons];
               if (type) {
                 return (
@@ -39,7 +29,10 @@ export default function InfoWrapper() {
               }
             })}
           </div>
-          {!!additionalInfo?.description && <div className="project-summary">{additionalInfo.description}</div>}
+          <div className="project-summary">
+            The mobile game immerses players in a metaverse that bridges the virtual and physical worlds, DEFY fuses
+            hyper casual code-breaking gameplay, with real world exploration and Augmented Reality (AR) adventures.
+          </div>
         </div>
         <div className="swiper-wrapper">swiper</div>
         <ProjectTabs />
--- src/pages/ProjectInfo/newComponents/ProjectTabs/index.tsx
+++ src/pages/ProjectInfo/newComponents/ProjectTabs/index.tsx
@@ -12,7 +12,26 @@ export default function ProjectTabs({ description }: { description?: string; IDO
     {
       key: ProjectTabsLabel.DESCRIPTION,
       label: ProjectTabsLabel.DESCRIPTION,
-      children: <div className="tabs-description-wrapper">{description}</div>,
+      children: (
+        <div className="tabs-description-wrapper">
+          The DEFY team believes that in order to make P&E sustainable for the long term, it is critical to wrap the
+          earning mechanics in a fun, highly engaging game which is rewarding beyond just playing to earn. Using a rich
+          narrative and introducing novel new features into the game, DEFY is built for the long term. Furthermore, the
+          DEFY game economy has been built to harness the creativity of the community, via the upcoming creators
+          platform which will allow users to burn $DEFY tokens in order to forge and create new NFT game assets which
+          may be sold via a native marketplace. By doing this, DEFY is creating a platform that has the ability to
+          absorb and distribute value in multiple ways. Location based play and earn game DEFY fuses hyper casual code
+          breaking gameplay, real world exploration and AR adventures The mobile game immerses players in a metaverse
+          that bridges the virtual and physical worlds, DEFY fuses hyper casual code-breaking gameplay, with real world
+          exploration and Augmented Reality (AR) adventures. DEFY Labs is proud to announce the completion of their
+          US$3.5m seed round led by Animoca Brands, liveXThe Spartan Group, GameFi Ventures, BIXIN, Polygon Studios,
+          Unanimous Capital, PathDAO andPlay It Forward DAO DEFY’s in-game economy has been designed with scale and
+          longevity in mind. A dual currency model is combined with an extensive set of customisable tradable game
+          assets as well as multiple active and passive earning mechanisms that can be leveraged by the players. A
+          creators platform will be added for content creators to collaborate with DEFY and bring NFTs into Augmented
+          Reality. DEFY intends to bring PvP into DEFY which creates huge longevity.
+        </div>
+      ),
     },
     {
       key: ProjectTabsLabel.IDO_INFORMATION,
